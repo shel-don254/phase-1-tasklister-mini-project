d2dbdf0e00e7955d2460e9f438402775af7b4033
sheldon <sheldonmwangi254@gmail.com>
2023-06-23 18:50:43 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  // alert("hi")
+  let add = document.querySelector('form')
+  add.addEventListener('submit', (event)=>{
+    event.preventDefault();
+   buildToDo(event.target.new.value);
+
+  //  clears the form after adding task
+   add.reset()
+  })
+
+  // creates the elements and listening events here
+  function buildToDo(todo){
+    let p = document.createElement('li');
+    let btn = document.createElement('button')
+    btn.addEventListener('click', handleDelete)
+    btn.textContent = 'x';
+    p.textContent = `${todo} `
+    document.querySelector('ul').appendChild(p)
+    p.appendChild(btn)
+  }
+
+  // removes the list
+  function handleDelete(e){
+e.target.parentNode.remove()
+  }
+ 
 });
